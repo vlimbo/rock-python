a6281a408a29fce76494d18713c933c75eafda03
Vetle Limbodal <vwlimbo@proton.me>
2024-10-30 20:24:00 +0100

    Create global counter for wins, loses and ties
    A global variable that keeps track of score is now in place as a global
    variable.

--- rock.py
+++ rock.py
@@ -4,6 +4,9 @@ def computerChoice():
     moves = ['r', 'p', 's']
     return random.choice(moves)
 
+
+wins, loses, ties = 0, 0, 0
+
 while True:
     userMove = input('(r)ock, (p)paper, (s)iccors, (q)uit ')
     computerMove = computerChoice()
@@ -13,25 +16,39 @@ while True:
     
     if userMove == computerMove:
         print('Tie!')
+        ties += 1
+        print('wins:', wins, 'loses:', loses, 'ties:', ties)
 
     elif userMove == 'r':
         if computerMove == 'p':
             print('You lose!')
+            loses += 1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
         else:
             print('You win!')
+            wins += 1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
 
     elif userMove == 'p':
         if computerMove == 'r':
             print('You win!')
+            wins += 1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
 
         else: 
             print('You lose!')
+            loses+= 1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
 
     elif userMove == 's':
         if computerMove == 'p':
             print('You win!')
+            wins += 1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
         else:
             print('You lose!')
+            loses +=1
+            print('wins:', wins, 'loses:', loses, 'ties:', ties)
     
     
 
