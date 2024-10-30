3de16f28b219697dc8e784473a9926ad67524295
Vetle Limbodal <vwlimbo@proton.me>
2024-10-30 19:40:14 +0100

    Create logic to compare user choice to computer
    With if and else statements we can compare the user choice to computer
    choice to see who wins and print the count to the screen.

--- rock.py
+++ rock.py
@@ -1,7 +1,37 @@
 import random
 
 def computerChoice():
-    moves = ['rock', 'paper', 'scissors']
+    moves = ['r', 'p', 's']
     return random.choice(moves)
 
-userMove = input('(r)ock, (p)paper, (s)iccors, (q)uit ')
+while True:
+    userMove = input('(r)ock, (p)paper, (s)iccors, (q)uit ')
+    computerMove = computerChoice()
+    
+    if userMove == 'q':
+        break
+    
+    if userMove == computerMove:
+        print('Tie!')
+
+    elif userMove == 'r':
+        if computerMove == 'p':
+            print('You lose!')
+        else:
+            print('You win!')
+
+    elif userMove == 'p':
+        if computerMove == 'r':
+            print('You win!')
+
+        else: 
+            print('You lose!')
+
+    elif userMove == 's':
+        if computerMove == 'p':
+            print('You win!')
+        else:
+            print('You lose!')
+    
+    
+
